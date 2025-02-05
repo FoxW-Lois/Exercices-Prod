d51ed91bb093a721ed7eb65ee22c69f50fdeaa40
FoxW-Lois <whaterfoxgaming@gmail.com>
2025-02-05 12:16:05 +0100

    Exercice 3 - Rajout Stacks

--- Exercice 3/MonProjet/app/(tabs)/index.tsx
+++ Exercice 3/MonProjet/app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import 'react-native-gesture-handler';
 
 export default function HomeScreen() {
 	return (
@@ -15,6 +16,7 @@ export default function HomeScreen() {
 				<ThemedText type="title">Welcome!</ThemedText>
 				<HelloWave />
 			</ThemedView>
+
 			<ThemedView style={styles.stepContainer}>
 				<ThemedText type="subtitle">Step 1: Try it</ThemedText>
 				<ThemedText>
@@ -29,10 +31,12 @@ export default function HomeScreen() {
 					to open developer tools.
 				</ThemedText>
 			</ThemedView>
+
 			<ThemedView style={styles.stepContainer}>
 				<ThemedText type="subtitle">Step 2: Explore</ThemedText>
 				<ThemedText>Tap the Explore tab to learn more about what's included in this starter app.</ThemedText>
 			</ThemedView>
+
 			<ThemedView style={styles.stepContainer}>
 				<ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
 				<ThemedText>
